9403b4519491bae940d80b01b5ab994c67c99987
Shane Galvin <Sgalvin36@gmail.com>
2024-08-16 15:41:36 -0600

    Passed dragon-tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,37 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  };
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
 
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten >= 3){
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+
+function findFireBreathers(array) {
+  let fireBreathers = []
+  for (const i of array){
+    if (i.temperment === 'aggressive'){
+      fireBreathers.push(i); 
+    }
+  }
+  return fireBreathers;
+}
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
